ca580d9f858fcfce0efd1c68e84eb1bf429d5b20
JHyeok <kjh2522k@gmail.com>
2019-05-18 01:46:41 +0900

    fix: 블로그 타이틀 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          title="Blog"
+          keywords={[`blog`, `backend`, `javascript`, `dotnet`, `dotnetcore`, `entityframework`, `development`]}
         />
         <Bio />
         {posts.map(({ node }) => {
