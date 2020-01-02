12636a9b098721b54602aaf9eebf0ddd4258a0d2
JHyeok <dev.jhyeok@gmail.com>
2020-01-03 00:29:09 +0900

    fix: footer 수정

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,7 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © 2019 - {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
