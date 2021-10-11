4c000742633a778d08669f83bee7188e4a0c2ad6
JaeHyeok Kim <dev.jhyeok@gmail.com>
2021-10-11 11:52:28 +0900

    fix: blockquote와 code의 css 수정
    다크 테마에서 글씨가 잘 보이지 않는 문제로 인하여 변경

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,16 +2,31 @@ import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 import './global.css'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
-      boxShadow: `none`,
-    },
-    a: {
-      color: `#8112ec`,
-    },
-  }
-}
+Wordpress2016.overrideThemeStyles = () => ({
+  a: {
+    color: `#8112ec`,
+  },
+  'a.gatsby-resp-image-link': {
+    boxShadow: `none`,
+  },
+  'p code': {
+    fontSize: '0.95rem',
+  },
+  'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code': {
+    fontSize: 'inherit',
+  },
+  'li code': {
+    fontSize: '0.95rem',
+  },
+  blockquote: {
+    color: 'inherit',
+    borderLeftColor: 'inherit',
+    opacity: '0.8',
+  },
+  'blockquote.translation': {
+    fontSize: '1em',
+  },
+})
 
 delete Wordpress2016.googleFonts
 
